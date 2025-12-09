9981f753c82869f4fab3542758b3013515190e7b
Nigel005 <p2862140@my365.dmu.ac.uk>
2025-12-09 14:46:31 +0000

    Created and added code to SW_RetreatState.cs

---
+++ GAMR2520-Assignment1-TankWars/Assets/YOURTEAMFOLDER - RENAME/Scripts/SW_RetreatState.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+using static AStar;
+
+public class SW_RetreatState : SW_BaseState
+{
+    private SW_SmartTank tank;
+    private GameObject enemyTank;
+
+    public HeuristicMode heuristicMode = HeuristicMode.Euclidean;
+
+    public float safeDistance = 40f;
+
+    public SW_RetreatState(SW_SmartTank tank)
+    {
+        this.tank = tank;
+    }
+
+    public override Type StateEnter()
+    {
+        Debug.Log("entered retreat state");
+        return typeof(SW_RetreatState);
+    }
+
+    public override Type StateExit()
+    {
+        return null;
+    }
+
+    public override Type StateUpdate()
+    {
+        // no visible enemies, return to patrol
+        if (tank.VisibleEnemyTanks.Count == 0)
+            return typeof(SW_PatrolState);
+
+        enemyTank = tank.VisibleEnemyTanks.First().Key;
+
+        if (enemyTank == null)
+            return typeof(SW_PatrolState);
+
+        float dist = Vector3.Distance(tank.transform.position, enemyTank.transform.position);
+
+        // if within safe distance, return to patrolling
+        if (dist > safeDistance)
+            return typeof(SW_PatrolState);
+
+        // retreat movement
+        Vector3 dirAway = (tank.transform.position - enemyTank.transform.position).normalized;
+        Vector3 retreatPoint = tank.transform.position + dirAway * 12f;
+
+        tank.FollowPathToWorldPoint(retreatPoint, 1f, heuristicMode);
+
+        return typeof(SW_RetreatState);
+    }
+}
