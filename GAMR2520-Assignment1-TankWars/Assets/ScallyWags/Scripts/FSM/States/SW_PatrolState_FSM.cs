153d4b2a14ea2fbba70ea11c94d246b41d988517
Axxlice <alusia465@gmail.com>
2025-12-12 15:29:23 +0000

    Comments

--- GAMR2520-Assignment1-TankWars/Assets/ScallyWags/Scripts/FSM/States/SW_PatrolState_FSM.cs
+++ GAMR2520-Assignment1-TankWars/Assets/ScallyWags/Scripts/FSM/States/SW_PatrolState_FSM.cs
@@ -5,20 +5,20 @@ using System;
 using System.Linq;
 using static AStar;
 
-public class SW_PatrolState_FSM : SW_BaseState_FSM
+public class SW_PatrolState_FSM : SW_BaseState_FSM // Patrol state references off base state
 {
     private SW_SmartTank_FSM tank;
     private float t;
 
     public SW_PatrolState_FSM(SW_SmartTank_FSM tank)
     {
-        this.tank = tank;
+        this.tank = tank; // Allows the use of all functions stored in the smart tank script
     }
 
     public override Type StateEnter()
     {
-        Debug.Log("Entered Patrol State");
-        t = 0;
+        Debug.Log("Entered Patrol State"); // Debug log which shows in the unity console whether the state has been entered
+        t = 0; // Timer set to 0
         return null;
     }
 
@@ -29,23 +29,23 @@ public class SW_PatrolState_FSM : SW_BaseState_FSM
 
     public override Type StateUpdate()
     {
-        if (tank.TankCurrentHealth < 30 || tank.TankCurrentFuel < 30 || tank.TankCurrentAmmo < 3)
+        if (tank.TankCurrentHealth < 30 || tank.TankCurrentFuel < 30 || tank.TankCurrentAmmo < 3) // If supplies are low (below specified numbers)
         {
-            return typeof (SW_ResupplyState_FSM);
+            return typeof (SW_ResupplyState_FSM); // Switch to resupply state
         }
-        if (tank.VisibleEnemyTanks.Count > 0 && tank.VisibleEnemyTanks.First().Key != null)
+        if (tank.VisibleEnemyTanks.Count > 0 && tank.VisibleEnemyTanks.First().Key != null) // If there's more than 0 visible enemy AND it is the first tank then enter chase state
         {
-            return typeof(SW_ChaseState_FSM);
+            return typeof(SW_ChaseState_FSM); // Switch to chase state
         }
         else
         {
-            tank.FollowPathToRandomWorldPoint(0.5f, tank.heuristicMode);
-            t += Time.deltaTime;
-            if (t > 10)
+            tank.FollowPathToRandomWorldPoint(0.5f, tank.heuristicMode); // Goes to a random world point using pathfinding
+            t += Time.deltaTime; // Timer starts
+            if (t > 10) // If timer goes over 10 seconds
             {
-                Debug.Log(t);
-                tank.GenerateNewRandomWorldPoint();
-                t = 0;
+                Debug.Log(t); // Displays time in unity console
+                tank.GenerateNewRandomWorldPoint(); // Generates new random world point to go to
+                t = 0; // Reset timer back to 0
             }
         }
         return typeof(SW_PatrolState_FSM);
--- GAMR2520-Assignment1-TankWars/Assets/ScallyWags/Scripts/FSM/States/SW_ResupplyState_FSM.cs
+++ GAMR2520-Assignment1-TankWars/Assets/ScallyWags/Scripts/FSM/States/SW_ResupplyState_FSM.cs
@@ -6,19 +6,19 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using static AStar;
 
-public class SW_ResupplyState_FSM : SW_BaseState_FSM
+public class SW_ResupplyState_FSM : SW_BaseState_FSM // Resupply state references off the Base state
 {
     private SW_SmartTank_FSM tank;
     float t;
 
     public SW_ResupplyState_FSM(SW_SmartTank_FSM tank)
     {
-        this.tank = tank;
+        this.tank = tank; // Reference to the Smart Tank script, which allows the use of all the functions stored in that script in other scripts
     }
 
     public override Type StateEnter()
     {
-        Debug.Log("Entered Resupply State");
+        Debug.Log("Entered Resupply State"); // Debug log, shows whether the state is entered. Great for testing as you can see if the tank is stuck in a state or not entering a different state.
         return typeof(SW_ResupplyState_FSM);
     }
 
@@ -29,27 +29,29 @@ public class SW_ResupplyState_FSM : SW_BaseState_FSM
 
     public override Type StateUpdate()
     {
-        
-         if (tank.VisibleConsumables.Count > 0)
+        // Overall this checks for visible consumables, if there are any it will go towards it.
+        // Otherwise it generates a new random point in the world (every 10 seconds) in order to cover most of the map to find consumables
+
+         if (tank.VisibleConsumables.Count > 0) // If there are visible consumables (more than 0)
          {
-             tank.consumable = tank.VisibleConsumables.First().Key;
-             tank.FollowPathToWorldPoint(tank.consumable, 0.8f, tank.heuristicMode);
-             t += Time.deltaTime;
-             if (t > 10)
+             tank.consumable = tank.VisibleConsumables.First().Key; // Get the first consumable from the list
+             tank.FollowPathToWorldPoint(tank.consumable, 0.8f, tank.heuristicMode); // Moves / follows toward the consumable
+             t += Time.deltaTime; // Timer
+             if (t > 10) // If timer goes over 10 seconds
              {
-                 tank.GenerateNewRandomWorldPoint();
-                 t = 0;
+                 tank.GenerateNewRandomWorldPoint(); // Generate new random point in the world
+                 t = 0; // Reset timer
              }
          }
-        else if (tank.TankCurrentHealth < 30 || tank.TankCurrentFuel < 30 || tank.TankCurrentAmmo < 3)
+        else if (tank.TankCurrentHealth < 30 || tank.TankCurrentFuel < 30 || tank.TankCurrentAmmo < 3) // Checks if all supplies are low
         {
             
-            tank.FollowPathToRandomWorldPoint(0.8f, tank.heuristicMode);
-            return typeof(SW_ResupplyState_FSM);
+            tank.FollowPathToRandomWorldPoint(0.8f, tank.heuristicMode); // Goes to random world point, in hopes of spotting a consumable
+            return typeof(SW_ResupplyState_FSM); // If it is then it stays in resupply state
         }
         else
         {
-            return typeof(SW_PatrolState_FSM);
+            return typeof(SW_PatrolState_FSM); // If the supplies are NOT low, it will switch to patrol state
         }
 
             return typeof(SW_ResupplyState_FSM);
